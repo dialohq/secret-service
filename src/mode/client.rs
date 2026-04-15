a0b58890face61ad6f2ea04668e8b1178861428d
plan9better <plan9better@proton.me>
2026-04-15 18:50:57 +0200

    feat(client): clean up send, listen, recv loop

--- src/mode/client.rs
+++ src/mode/client.rs
@@ -23,46 +23,20 @@ impl Client {
 
         let socket = Arc::new(UdpSocket::bind(format!("{}:{}", self.bind, self.port)).await?);
         socket.set_broadcast(true)?;
+        let mut buf = [0u8; 4096];
+        loop {
+            info!(?self.target, "Sending broadcast");
+            socket.send_to(hostname.as_bytes(), &self.target).await?;
 
-        let recv_s = socket.clone();
-        let snd_s = socket.clone();
-
-        let listener_handle = tokio::spawn(async move {
-            let mut buf = [0u8; 4096];
-            info!("Waiting for reply");
-            loop {
-                let (n, src) = recv_s.recv_from(&mut buf).await?;
-                let src = src.to_string();
-                let msg = std::str::from_utf8(&buf[..n])?;
-                info!(msg, src, "Message received");
-                break;
+            tokio::select! {
+                Ok((n, src)) = socket.recv_from(&mut buf) => {
+                    info!(len = n, src = %src, "Message received");
+                    break Ok(());
+                },
+                _ = tokio::time::sleep(std::time::Duration::from_secs(5)) => {
+                    continue;
+                }
             }
-            Ok::<(), anyhow::Error>(())
-        });
-        let target = self.target.clone();
-        let discover_handle = tokio::spawn(async move {
-            loop {
-                info!(?target, "Sending broadcast");
-                snd_s.send_to(hostname.as_bytes(), &target).await?;
-                tokio::time::sleep(std::time::Duration::from_secs(5)).await;
-            }
-            // Just for type inference
-            #[allow(unreachable_code)]
-            Ok::<(), anyhow::Error>(())
-        });
-
-        info!("waiting for reply...");
-        let _: Result<Result<()>> = tokio::select! {
-            _ = discover_handle => {
-                info!("discover handle finished");
-                Ok(Ok(()))
-            },
-            _ = listener_handle => {
-                info!("listener handle finished (got reply)");
-                Ok(Ok(()))
-            },
-        };
-        info!("shutting down");
-        Ok(())
+        }
     }
 }
