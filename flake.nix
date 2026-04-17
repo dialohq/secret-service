5ee7f2e7895c153bb7dcda1f3809e017810eadba
plan9better <plan9better@proton.me>
2026-04-17 11:08:22 +0200

    feat(nix): build with naersk

--- flake.nix
+++ flake.nix
@@ -2,28 +2,32 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     nixpkgs,
     flake-utils,
+    naersk,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = import nixpkgs {inherit system;};
+        naersk' = pkgs.callPackage naersk {};
       in {
         nixosModules.default = import ./nix/module.nix;
         packages = {
-          default = pkgs.rustPlatform.buildRustPackage {
-            pname = "secretservice";
-            version = "0.1";
-            cargoLock.lockFile = ./Cargo.lock;
-            src = pkgs.lib.cleanSource ./.;
+          default = naersk'.buildPackage {
+            src = ./.;
+            buildInputs = [pkgs.sqlite];
           };
         };
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [cargo rustc clippy rustfmt rust-analyzer];
+          packages = with pkgs; [cargo rustc clippy rustfmt rust-analyzer sqlite];
         };
       }
     );
